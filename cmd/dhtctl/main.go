1ad476bc73c5bc1aabe08cd6b70a88233ff9c3c3
edvinnn <sladic.edvin@gmail.com>
2019-09-04 18:33:36 +0200

    dhtctl: defined functions for CLI

--- cmd/dhtctl/main.go
+++ cmd/dhtctl/main.go
@@ -3,17 +3,57 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 )
 
-var ipText = flag.String("address", "localhost", "the ip-address of the node")
-var put = flag.String("put", "", "put value to store")
-var get = flag.String("get", "", "key of the value to get")
-var ping = flag.Bool("ping", false, "ping the node at address")
-var exit = flag.Bool("exit", false, "terminate the node")
+func strToIP(str string) net.IP {
+	return net.ParseIP(str)
+}
+
+func executePut() {
+
+}
+
+func executeGet() {
+
+}
+
+func executePing() {
+
+}
+
+func executeExit() {
+
+}
 
 func main() {
+	// Flags
+	var ip = flag.String("address", "localhost", "the ip-address of the node")
+	var put = flag.String("put", "", "put value to store")
+	var get = flag.String("get", "", "key of the value to get")
+	var ping = flag.Bool("ping", false, "ping the node at address")
+	var exit = flag.Bool("exit", false, "terminate the node")
+
+	// Parse input and validate ip-address from CLI
 	flag.Parse()
-	fmt.Println("IP: ", *ipText)
+	// TODO: netIP, err := strToIP(ip)
+
+	// Execute tasks given via CLI
+	if "" != *put {
+		executePut()
+	}
+	if "" != *get {
+		executeGet()
+	}
+	if *ping {
+		executePing()
+	}
+	if *exit {
+		executeExit()
+	}
+
+	// Debug output
+	fmt.Println("IP: ", *ip)
 	fmt.Println("Put: ", *put)
 	fmt.Println("Get: ", *get)
 	fmt.Println("Ping :", *ping)
