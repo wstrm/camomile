6bca6fde2aa8ec3fd8478c0be98aef5a8e3b0aa9
edvinnn <sladic.edvin@gmail.com>
2019-09-04 17:42:13 +0200

    dhtctl: started working on cli

--- cmd/dhtctl/main.go
+++ cmd/dhtctl/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var ipText = flag.String("address", "localhost", "the ip-address of the node")
+var put = flag.String("put", "", "put value to store")
+var get = flag.String("get", "", "key of the value to get")
+var ping = flag.Bool("ping", false, "ping the node at address")
+var exit = flag.Bool("exit", false, "terminate the node")
 
 func main() {
-	fmt.Println("dhtctl")
+	flag.Parse()
+	fmt.Println("IP: ", *ipText)
+	fmt.Println("Put: ", *put)
+	fmt.Println("Get: ", *get)
+	fmt.Println("Ping :", *ping)
+	fmt.Println("Exit: ", *exit)
 }
