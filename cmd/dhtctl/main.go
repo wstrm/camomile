c90c0475fc8b65851bbe530e75d689896d9ff7f9
edvinnn <sladic.edvin@gmail.com>
2019-09-04 17:42:13 +0200

    dhtctl: started working on cli
    dhtctl: defined functions for CLI
    
    dhtctl: parsing from cli to type UDPAddr
    
    dhtctl: Added som kind of validation for IP address parsing
    
    dhtctl: Added keys to fields in updAddr struct init
    
    dhtctl: Resolved change requests
    
    dhtctl: signed commit
    
    second try at signed commit
    
    thrid atempt at signed commit
    
    squash all as signed commit

--- cmd/dhtctl/main.go
+++ cmd/dhtctl/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+func executePut() {
+	//TODO
+}
+
+func executeGet() {
+	//TODO
+}
+
+func executePing() {
+	//TODO
+}
+
+func executeExit() {
+	//TODO
+}
 
 func main() {
-	fmt.Println("dhtctl")
+	// Flags
+	var address = flag.String("address", "localhost", "the address of the node")
+	var put = flag.String("put", "", "put value to store")
+	var get = flag.String("get", "", "key of the value to get")
+	var ping = flag.Bool("ping", false, "ping the node at address")
+	var exit = flag.Bool("exit", false, "terminate the node")
+
+	// Parse input and validate address
+	flag.Parse()
+	udpAddr, err := net.ResolveUDPAddr("udp", *address)
+	if err != nil {
+		log.Fatalln("Unable to parse address.")
+	}
+
+	// Execute tasks
+	if "" != *put {
+		executePut()
+	}
+	if "" != *get {
+		executeGet()
+	}
+	if *ping {
+		executePing()
+	}
+	if *exit {
+		executeExit()
+	}
+
+	// Debug output
+	fmt.Println("IP: ", udpAddr)
+	fmt.Println("Put: ", *put)
+	fmt.Println("Get: ", *get)
+	fmt.Println("Ping :", *ping)
+	fmt.Println("Exit: ", *exit)
 }
