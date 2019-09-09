9dfb0de8f4afa600c44ea4470bb3febc2c754ec7
Philip Hjortsberg <Hjortsberg@users.noreply.github.com>
2019-09-09 08:46:40 +0200

    dhtnode: simple server with send and listen (#16)
    * dhtnode: simple server with send and listen
    * cmd: moved udptetclient and corrected style

--- cmd/dhtnode/main.go
+++ cmd/dhtnode/main.go
@@ -1,7 +1,57 @@
 package main
 
 import "fmt"
+import "net"
+import "log"
+
+func handlePacket(conn *net.UDPConn) {
+	// TODO: implement what happens with received messages.
+	// go-routines go here and connect to channel for message proccessing?
+}
+
+// sendPacket takes packet bytes, and an UDP address.
+// The packet bytes must be serialized using Protobuf beforehand.
+func sendPacket(packet []byte, address *net.UDPAddr) {
+	conn, _ := net.DialUDP("udp", nil, address)
+	defer conn.Close()
+	_, err := conn.Write(packet)
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func main() {
 	fmt.Println("dhtnode")
+
+	// Listen to all addresses on port 8118.
+	udpAddress, err := net.ResolveUDPAddr("udp", ":8118")
+	if err != nil {
+		log.Fatalf("Unable to resolve IP and Port, %v", err)
+	}
+	conn, err := net.ListenUDP("udp", udpAddress)
+	if err != nil {
+		log.Fatalf("Unable to listen at %v, %v", udpAddress.String(), err)
+		return
+	}
+
+	defer conn.Close()
+
+	// Say hello to yourself, this should probably be changed to a node in the network.
+	sendPacket([]byte("Hello, I'm a node"), &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: 8118, Zone: ""})
+
+	for {
+		fmt.Println("Listening for UDP packets on port 8118")
+		data := make([]byte, 512)
+
+		n, addr, err := conn.ReadFromUDP(data)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		s := string(data[:n])
+
+		// Print the request data.
+		fmt.Println("UDP client sent a message from: ", addr)
+		fmt.Println(s)
+		go handlePacket(conn)
+	}
 }
