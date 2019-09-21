61c76e82a5f7012b89420790969e25eeb2f07b62
Philip Hjortsberg <philip@hjortsberg.me>
2019-09-21 11:47:13 +0200

    store: concurrent datastructures, AddItem test

--- store/store.go
+++ store/store.go
@@ -1,55 +1,80 @@
 package store
 
-import "fmt"
-import "log"
+//import "log"
 import "time"
 import "golang.org/x/crypto/blake2b"
+import "sync"
 
-type NodeID [20]byte
+type NodeID [32]byte
 type Key NodeID
 
+var db *database
+
 type item struct {
-	value		[255]byte
-	expire		time.Time
-	republish	time.Time
-	origPub		NodeID
+	value     string
+	expire    time.Time
+	republish time.Time
+	origPub   NodeID
 }
 
-type items map[Key]item
-type storedKeys map[Key]time.Time
+type items struct {
+	sync.RWMutex
+	m map[Key]item
+}
 
-func (i *items) add(value [255]byte, origPub NodeID) (error) {
+type keys struct {
+	sync.RWMutex
+	m map[Key]time.Time
+}
+
+type database struct {
+	items	items
+	keys	keys
+	ch	chan(Key)
+}
+
+func init() {
+	db = new(database)
+	db.items = items{m: make(map[Key]item)}
+	db.keys = keys{m: make(map[Key]time.Time)}
+	db.ch = make(chan Key)
+}
+
+// TODO: Max 1000 chars. Truncate input string.
+func AddItem(value string, origPub NodeID) error {
 	t := time.Now()
 	expire := t.Add(time.Second * 86400)
 	republish := expire
 
-	h, err := blake2b.New(160, value)
-	if err != nil {
-		return err
-	}
-	key := h.sum()
+	var key Key
+	key = blake2b.Sum256([]byte(value))
 
-	newItem := new(item)
+	newItem := item{}
 
 	newItem.value = value
 	newItem.expire = expire
 	newItem.republish = republish
 	newItem.origPub = origPub
 
-	i[key] = newItem
+	// TODO: Handle duplicate keys.
+	db.items.Lock()
+	db.items.m[key] = newItem
+	db.items.Unlock()
+
+	return nil
 }
 
-func (k *storedKeys) add(key Key) {
+
+
+func AddKey(key Key) {
 	t := time.Now()
 	republish := t.Add(time.Second * 86400)
 
-	newStoredKey := new(storedKey)
-
-	newStoredKey.key = key
-	newStoredKey.republish = republish
-
-	k[key] = newStoredKey
+	db.keys.Lock()
+	db.keys.m[key] = republish
+	db.keys.Unlock()
 }
+
 /*
 func (i *items) evict(key Key) {
 	delete(*items, key)
@@ -68,7 +93,7 @@ func (i *items) itemHandler() {
 	}
 }
 
-// Start as Goroutine. 
+// Start as Goroutine.
 // Function is responsible for republishing data that should persist on storage network.
 func (k *storedKeys) republisher(repub chan storedKey) {
 	timer := time.NewTimer(time.Second * 1)
--- store/store_test.go
+++ store/store_test.go
@@ -1,40 +1,38 @@
 package store
 
 import "testing"
+//import "fmt"
+//import "golang.org/x/crypto/blake2b"
+//import "encoding/hex"
 
+func TestItemsAdd(t *testing.T) {
+	testVal := "q"
 
-func testBlake2b(){
-	// After testItemsAdd has ran, the stored hash should be the 
-	// value of 36ff942f90c129cfe207dc3063f4cd19aaeda211. 
-	// This is the hex representation of "q" hashed with blake2b-160.
-
-	if !testItemsMap.
-}
-
-func populateTestItem() {
-	testItem := item
-
-	// "q" translates to 01110001 in binary.
-	v := "q"
-	copy(testItem.value[:], v)
-
-	testItem.expire = time.Now().Add(time.Second * 2)
-	testItem.republish = time.Now().Add(time.Second * 2)
-
-	// "w" translates to 01110111 in binary.
-	s := "w"
-	copy(testItem.origPub[:], s)
-}
-
-func testItemsAdd() {
-	testVal := [255]byte
-	copy(testVal[:], "q")
-
-	testNodeID := NodeID
+	var testNodeID NodeID
 	copy(testNodeID[:], "w")
+	err := AddItem(testVal, testNodeID)
+	if err != nil {
+		t.Errorf("some error in AddItem that is not yet defined")
+	}
 
-	testItemsMap := items
-	testItemsMap.add(testVal, testNodeID)
+	trueHash := [32]byte{174, 79, 167, 92, 82, 249, 190, 142, 129, 67, 178, 149, 52, 212, 158, 150, 67, 136, 83, 10, 170, 233, 83, 34, 158, 194, 62, 241, 14, 168, 19, 103}
 
-	if !testItemsMap[// TODO: extract first key] == the true value of q hashed with blake2b-160
+	db.items.RLock()
+	storedTestItem, found := db.items.m[trueHash]
+	db.items.RUnlock()
+	if !found {
+		t.Errorf("did not find entry in hash table for: %x", trueHash)
+	}
+
+	if !(testVal == storedTestItem.value) {
+		t.Errorf("value of item does not match original value.\nExpected: %x\nGot: %x", testVal, storedTestItem.value)
+	}
+
+	if !(testNodeID == storedTestItem.origPub) {
+		t.Errorf("NodeID stored does not match original NodeID.\nExpected: %x\nGot: %x", testNodeID, storedTestItem.origPub)
+	}
 }
+
+/*func testStoredKeysAdd(){
+
+}*/
