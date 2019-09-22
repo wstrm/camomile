3e0b8f0b4ab6703ff06171625e2012b458d098e8
edvinnn <sladic.edvin@gmail.com>
2019-09-22 17:47:58 +0200

    network: store seems to be working

--- network/network.go
+++ network/network.go
@@ -10,34 +10,72 @@ import (
 	"net"
 )
 
-type key node.ID
+const UdpPort  = ":8118"
 
-func Hash(value string) key {
+type Key node.ID
+
+func Hash(value string) Key {
 	var key = blake2b.Sum256([]byte(value))
 	return key
 }
 
-func Store(addr *net.UDPAddr, senderID []byte, value string) {
-	// Generate packetID
-	id := make([]byte, 4)
-	_, err := rand.Read(id)
+func ListenUDP(c chan string){
+	udpAddr, err := net.ResolveUDPAddr("udp", UdpPort)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Unable to resolve UDP address", err)
 	}
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatalln("Unable to listen at UDP address", err)
+	}
+	defer conn.Close()
 
+	for {
+		log.Printf("Listening for UDP packets on port %v", UdpPort)
+
+		data := make([]byte, 1500)
+		n, addr, err := conn.ReadFromUDP(data)
+		if err != nil {
+			log.Fatalf("Error when reading from UDP from address %v: %s", addr.String(), err)
+		}
+		b := data[:n]
+		go handlePacket(b, c)
+	}
+}
+
+func Store(addr *net.UDPAddr, senderID []byte, value string) {
+	id := generateID()
 	key := Hash(value)
 
 	payload := &packet.Store{
 		Key:                  key[:],
 		Value:                value,
 	}
-	packet := &packet.Packet{
+	p := &packet.Packet{
 		PacketId:             id,
 		SenderId:             senderID,
 		Payload:              &packet.Packet_Store{Store: payload},
 	}
 
-	send(addr, *packet)
+	send(addr, *p)
+}
+
+func handlePacket(b []byte, c chan string) {
+	p := &packet.Packet{}
+	err := proto.Unmarshal(b, p)
+	if err != nil {
+		log.Fatalln("Error unserializing packet", err)
+	}
+	c <- p.GetStore().Value
+}
+
+func generateID() []byte {
+	id := make([]byte, node.IDBytesLength)
+	_, err := rand.Read(id)
+	if err != nil {
+		log.Fatalln("Error generating packetID", err)
+	}
+	return id
 }
 
 func send(addr *net.UDPAddr, packet packet.Packet) {
@@ -54,6 +92,7 @@ func send(addr *net.UDPAddr, packet packet.Packet) {
 
 	_, err = conn.Write(b)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Error writing via UDP", err)
 	}
 }
+
--- network/network_test.go
+++ network/network_test.go
@@ -2,32 +2,26 @@ package network_test
 
 import (
 	"github.com/optmzr/d7024e-dht/network"
-	"github.com/optmzr/d7024e-dht/packet"
 	"log"
 	"net"
 	"testing"
 )
 
+const value  = "ABC, du är mina tankar."
+
 func TestStore(t *testing.T) {
-	payload := &packet.Store{
-		Key:                  []byte{111},
-		Value:                "ABC, du är mina tankar",
-	}
-
-	r := &packet.Packet{
-		PacketId: []byte{123},
-		SenderId: []byte{100},
-		Payload: &packet.Packet_Store{payload},
-	}
-
-	udpAddress, err := net.ResolveUDPAddr("udp", ":8118")
-
-	network.Send(udpAddress, r)
-
-	conn, err := net.ListenUDP("udp", udpAddress)
+	udpAddress, err := net.ResolveUDPAddr("udp", network.UdpPort)
 	if err != nil {
-		log.Fatalf("Unable to listen at %v, %v", udpAddress.String(), err)
-		return
+		log.Fatalln("Unable to resolve UDP address", err)
 	}
-	defer conn.Close()
+
+	ch := make(chan string)
+	go network.ListenUDP(ch)
+
+	network.Store(udpAddress, []byte{100}, value)
+	s := <- ch
+	log.Println(s)
+	if s != value {
+		t.Errorf("Expected: %s, got: %s", value, s)
+	}
 }
