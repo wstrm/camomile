d604e8d955199e2598c247132dffa3315e119a84
William Wennerström <william@optmzr.se>
2019-09-28 18:08:56 +0200

    doc: fix README.md

---
+++ bin/run-node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -euo pipefail
+
+menum=$1
+meid=$2
+meaddr=$3
+
+otherid=$4
+otheraddr=$5
+
+echo "I am: #$menum, $meid@$meaddr"
+dhtnode -me "$meid@$meaddr" -other "$otherid@$otheraddr"
