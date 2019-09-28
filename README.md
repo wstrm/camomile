e0b73d116437180103e5e266c8253998b5a4f64c
William Wennerström <william@optmzr.se>
2019-09-28 22:13:06 +0200

    bin: write custom script instead of using docker-compose (#46)
    Remove docker-compose because I suck and I couldn't figure out how to
    pair the nodes.
    
    * doc: fix README.md

--- README.md
+++ README.md
@@ -5,3 +5,30 @@ Camomile
 | Build status | Test coverage | Camomile docs |
 |:------------:|:-------------:|:--------------|
 | [![Build Status](https://circleci.com/gh/optmzr/camomile.svg?style=svg)](https://circleci.com/gh/optmzr/camomile) | [![Code Coverage](https://codecov.io/gh/optmzr/camomile/branch/master/graph/badge.svg)](https://codecov.io/gh/optmzr/camomile) | [![Camomile Documentation](https://godoc.org/github.com/optmzr/camomile?status.svg)](https://godoc.org/github.com/optmzr/camomile) |
+
+## Build Camomile
+Download and install Protoc into `/usr/local`:
+```
+curl -sfL -o /tmp/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.9.1/protoc-3.9.1-linux-x86_64.zip
+sudo unzip /tmp/protoc.zip -d /usr/local/  # Dangerous!!!
+```
+
+Get dependencies, generate and build the binaries:
+```
+go get -u github.com/golang/protobuf/protoc-gen-go
+go generate ./...
+go build ./cmd/...
+```
+
+## Run as cluster
+Build the Docker container:
+```
+docker build . -t dhtnode:latest
+```
+
+Start the cluster script:
+```
+./bin/run-cluster.sh <num> # Change <num> to the number of nodes to run.
+```
+
+Done!
---
+++ bin/run-cluster.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    echo "Please pass the number of nodes as an argument"
+    exit 1
+fi
+
+numnodes=$1
+networkname="camomilenet"
+networkprefix="172.22.0"
+port="8118"
+
+docker network create --subnet="$networkprefix.0/16" "$networkname" 2>&1
+
+set -e
+
+cleanup() {
+    echo "Exiting, please wait..."
+    docker ps --filter network="$networkname" -q \
+        | xargs -P "$numnodes" -L 1 docker stop
+    echo "Bye, have a good day! :)"
+}
+trap cleanup EXIT
+
+genhash() {
+    printf "%s" "$1" | sha256sum | cut -f1 -d' '
+}
+
+lastnum="$numnodes"
+lastip="$networkprefix.$((numnodes+1))"
+lastid=$(genhash "$lastnum")
+
+# Start N nodes and pair them.
+for num in $(seq 1 "$numnodes")
+do
+    nodeip="$networkprefix.$((num+1))"
+
+    # Generate node ID with node number.
+    nodeid=$(genhash "$num")
+
+    echo "Starting node: #$num, $nodeid@$nodeip:$port"
+    docker run --net "$networkname" --ip "$nodeip" -t -d \
+        dhtnode "$num" "$nodeid" "$nodeip:$port" \
+                "$lastid" "$lastip:$port" >/dev/null &
+
+    lastnum="$num"
+    lastid="$nodeid"
+    lastip="$nodeip"
+done
+
+printf "Waiting for all nodes to finish starting up... "
+wait  # Wait for all sub processes to finish (docker run).
+echo "Done!"
+
+# Print and follow all the logs from the nodes.
+docker ps -q | xargs -L 1 -P "$numnodes" docker logs --details --follow
